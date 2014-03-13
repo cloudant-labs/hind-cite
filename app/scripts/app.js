a44ce065b95189fe7a9f91271478e651985bdb8b
Ross Rosen <rrosen326@gmail.com>
2014-03-12 18:37:06 -0700

    Revert back to before all the requirejs work.
    Revert "Interim - lots of work (mostly ineffective!) getting requirejs to work."
    
    This reverts commit 224829214b853d125e2bbaa753d58e5bf68a13cb.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,29 +1,18 @@
 'use strict';
 
-define(['angular', 'angularRoute', 'scripts/controllers/main'], function() {
-
-    console.log('scripts/app.js running!');
-    angular.module('hnApp', ['ngRoute', 'MainCtrlProvider'])
-        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-            $locationProvider.html5Mode(true);
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'views/main.html',
-                    controller: 'MainCtrl'
-                })
-                .when('/test', {
-                    templateUrl: 'views/test.html',
-                    controller: 'MainCtrl'
-                })
-                .otherwise({
-                    redirectTo: '/'
-                });
-        }]);
-
-
-    // Return
-    return {
-
-    };
-
-});
+angular.module('hnApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,10 @@
 'use strict';
 
-define(['angular'], function () {
-
-    // Init code
-    var mainCtrlProvider=angular.module('MainCtrlProvider', []);
-
-
-    mainCtrlProvider
-        .controller('MainCtrl', function ($scope) {
-            $scope.awesomeThings = [
-                'HTML5 Boilerplate',
-                'AngularJS',
-                'Karma'
-            ];
-        });
-
-    // Return
-    return {
-
-    }
-
-});
+angular.module('hnApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
