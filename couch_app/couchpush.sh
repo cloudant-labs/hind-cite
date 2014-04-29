043d00cd946ac481a28da98432cbe690a8cf9749
Ross Rosen <rrosen326@gmail.com>
2014-04-29 14:20:35 -0700

    couchapp working

--- couch_app/couchpush.sh
+++ couch_app/couchpush.sh
@@ -1,12 +1,26 @@
-#!/bin/sh
+#!/opt/local/bin/zsh
 
 echo
 echo '****************************'
 echo 'Update production couchapp'
 echo '****************************'
 
+USE_SOURCE="False"
 
-echo 'Going to use SOURCE code (not dist) for push to couchapp'
 
-cp -rf ../app/*  ./_attachments
+if [[ $USE_SOURCE = "True" ]]; then
+        echo 'Going to use SOURCE code (not dist) for push to couchapp'
+        SOURCE_DIR="../app/"
+    else
+        echo 'Going to use DIST'
+        SOURCE_DIR="../dist/"
+fi
+
+echo "Rsyncing $SOURC_DIR to _attachments"
+rsync -ra --stats $SOURCE_DIR  _attachments/
+
+echo
+echo "couchapp push . prod"
 couchapp push . prod
+
+echo 'Done'
