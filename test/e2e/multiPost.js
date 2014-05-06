0a1d7f6c5da5c5e0454f70113971cc2d76303ef9
Ross Rosen <rrosen326@gmail.com>
2014-05-06 06:46:10 -0700

    interim

--- test/e2e/multiPost.js
+++ test/e2e/multiPost.js
@@ -23,7 +23,7 @@ function compareCol(colModelText, compareFn) {
     colArValsP.then(function (colArVals) {
         var retVal = compareFn(colArVals);
         if (!retVal) {
-            console.log(colModelText + ' compareFn returning false. Column data:', colArVals);
+            console.log('table (' + colModelText + ') compareFn returning false. Column data:', colArVals);
         }
         retValD.fulfill(retVal);
     });
@@ -54,42 +54,47 @@ function WaitForState(objName, stateIsTrue) {
 
 
 function testTable(expNumPosts, shouldTitle, colModelText, compareFn) {
-    describe('multiPost data table', function () {
 
 
-        // Wait til table is initialized with data
-        var waitFn = new WaitForState('data', 'dataUpdated');
-        browser.wait(waitFn, 5000, 'multiPost failed to load data properly');
+    // Wait til table is initialized with data
+    var waitFn = new WaitForState('data', 'dataUpdated');
+    browser.wait(waitFn, 5000, 'multiPost failed to load data properly')
+        .then(function () {
+            console.log('testTable after wait ', expNumPosts, shouldTitle, colModelText);
 
-        it('should have a ' + expNumPosts + '  lines', function () {
-            expect(element.all(by.css('#postDataTable tbody tr')).count()).toBe(expNumPosts);
+            describe('test table', function () {
+                it('table should have a ' + expNumPosts + '  lines', function () {
+                    expect(element.all(by.css('#postDataTable tbody tr')).count()).toBe(expNumPosts);
+                    console.log('im here');
 
-        });
+                });
 
-        it(shouldTitle, function () {
-            compareCol(colModelText, compareFn).then(function (retVal) {
-                expect(retVal).toBe(true);
+                it(shouldTitle, function () {
+                    compareCol(colModelText, compareFn).then(function (retVal) {
+                        expect(retVal).toBe(true);
+                        console.log('and im here');
+                    });
+
+                });
             });
 
         });
-    });
+
 }
 
 function testChart(expNumPosts, sortFn) {
-    describe('multiPost chart', function () {
 
-        var expNumPosts = 10;
+    var expNumPosts = 10;
 
-        // Wait til chart is initialized with data
-        var waitFn = new WaitForState('chart', 'chartUpdated');
-        browser.wait(waitFn, 5000, 'multiPost failed to load chart properly');
+    // Wait til chart is initialized with data
+    var waitFn = new WaitForState('chart', 'chartUpdated');
+    browser.wait(waitFn, 5000, 'multiPost failed to load chart properly');
 
-        it('should have a ' + expNumPosts + '  lines', function () {
-            expect(element.all(by.css('.nv-group')).count()).toBe(2 * expNumPosts);  // one for stroke, one for fill
-        });
-
-
+    it('chart should have a ' + expNumPosts + '  lines', function () {
+        expect(element.all(by.css('.nv-group')).count()).toBe(2 * expNumPosts);  // one for stroke, one for fill
     });
+
+
 }
 
 describe('hind-cite multiPost', function () {
@@ -132,13 +137,13 @@ describe('hind-cite multiPost', function () {
         });
 
 
-        testTable(10, 'lastRank should be ordered 1 to 10', 'stats.lastRank', function (inAr) {
+        testTable(10, 'table (lastRank) should be ordered 1 to 10', 'stats.lastRank', function (inAr) {
             return _und.isEqual(inAr, ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
         });
         testChart(10, null);
     });
 
-    describe('most points', function () {
+    xdescribe('most points', function () {
         browser.get('/multiPost');
 
         var options = element.all(by.tagName('option'));
@@ -152,7 +157,7 @@ describe('hind-cite multiPost', function () {
             });
         });
 
-        testTable(10, 'most points should have decreasing points', 'stats.maxPoints', function (inAr) {
+        testTable(10, 'table (most points) should have decreasing points', 'stats.maxPoints', function (inAr) {
             // each val should be <= previous (note - inAr are strings, eg: '1,323'
             var retVal = true,
                 lastVal = Number(inAr[0].replace(/,/g, '')),
@@ -167,79 +172,3 @@ describe('hind-cite multiPost', function () {
     });
 });
 
-
-
-
-
-
-xdescribe('hind-cite multiPost', function () {
-    var waitFn = new WaitForState('data', 'dataUpdated');
-
-        browser.get('/multiPost');
-
-        n.testNavbar();
-
-        it('should have a post selector section', function () {
-            expect(element(by.id('postsSelectors')).isPresent()).toBe(true);
-        });
-
-        it('should a chart control section', function () {
-            expect(element(by.id('chartControls')).isPresent()).toBe(true);
-        });
-
-        it('should have a Post Data Table ', function () {
-            expect(element(by.id('postDataTable')).isPresent()).toBe(true);
-        });
-
-        it('should have a postChart', function () {
-            expect(element(by.id('postsChart')).isPresent()).toBe(true);
-        });
-
-
-        browser.get('/multiPost');
-        var options = element.all(by.tagName('option'));
-        options.each(function (elm, idx) {
-            elm.getAttribute('value').then(function (text) {
-                if (text === '["top","all"]') {
-                    console.log('found HN Front Page - clicking');
-                    elm.click();
-                }
-
-            });
-        });
-        browser.wait(waitFn, 5000, 'multiPost failed to load data properly');
-
-
-        testTable(10, 'lastRank should be ordered 1 to 10', 'stats.lastRank', function (inAr) {
-            return _und.isEqual(inAr, ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']);
-        });
-        testChart(10, null);
-
-        browser.get('/multiPost');
-
-        var options = element.all(by.tagName('option'));
-        options.each(function (elm, idx) {
-            elm.getAttribute('value').then(function (text) {
-                if (text === '["points","all"]') {
-                    console.log('found points all - clicking');
-                    elm.click();
-                }
-
-            });
-        });
-
-        browser.wait(waitFn, 5000, 'multiPost failed to load data properly');
-
-        testTable(10, 'most points should have decreasing points', 'stats.maxPoints', function (inAr) {
-            // each val should be <= previous (note - inAr are strings, eg: '1,323'
-            var retVal = true,
-                lastVal = Number(inAr[0].replace(/,/g, '')),
-                val;
-            inAr.forEach(function (str) {
-                val = Number(str.replace(/,/g, ''));
-                retVal = retVal && val <= lastVal;
-            });
-            return retVal;
-        });
-        testChart(10, null);
-});
